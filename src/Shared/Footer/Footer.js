958da47120a4a15c928ec2272c4f37507a75a58d
devmonika <devmonikamomi@gmail.com>
2022-12-30 23:08:48 +0600

    footer added

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-10 bg-primary text-primary-content">
+            <div>
+                <h1 className='text-2xl font-bold'>WriteBook</h1>
+                
+                <p>Copyright © 2022 - All right reserved</p>
+            </div> 
+            
+            </footer>
+    );
+};
+
+export default Footer;
